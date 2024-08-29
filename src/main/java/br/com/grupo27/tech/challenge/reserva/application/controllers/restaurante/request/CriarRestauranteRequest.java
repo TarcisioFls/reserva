ea38d8ea5baa1c4d96909044496e0499bf77ba63
Waldir Júnior <waldirjunior.gpn@gmail.com>
2024-08-29 14:00:56 -0300

    Implementa as camadas Gateway e Presenter de Restaurante

--- src/main/java/br/com/grupo27/tech/challenge/reserva/application/controllers/restaurante/request/CriarRestauranteRequest.java
+++ src/main/java/br/com/grupo27/tech/challenge/reserva/application/controllers/restaurante/request/CriarRestauranteRequest.java
@@ -1,23 +1,24 @@
 package br.com.grupo27.tech.challenge.reserva.application.controllers.restaurante.request;
 
 import br.com.grupo27.tech.challenge.reserva.domain.entity.Proprietario;
+import br.com.grupo27.tech.challenge.reserva.domain.entity.TipoCozinha;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CriarRestauranteRequest {
 
-    private String id;
     private String nome;
     private String descricao;
     private String localizacao;
     private LocalDateTime horarioFuncionamento;
     private int capacidade;
-    //    private List<TipoCozinha> tipoCozinha;
+    private List<TipoCozinha> tipoCozinha;
     private Proprietario proprietario;
 }
--- src/main/java/br/com/grupo27/tech/challenge/reserva/domain/output/restaurante/CriarRestauranteOutput.java
+++ src/main/java/br/com/grupo27/tech/challenge/reserva/domain/output/restaurante/CriarRestauranteOutput.java
@@ -1,24 +1,24 @@
 package br.com.grupo27.tech.challenge.reserva.domain.output.restaurante;
 
 import br.com.grupo27.tech.challenge.reserva.domain.entity.Proprietario;
-import br.com.grupo27.tech.challenge.reserva.domain.input.restaurante.CriarRestauranteInput;
+import br.com.grupo27.tech.challenge.reserva.domain.entity.TipoCozinha;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CriarRestauranteOutput {
 
-    private String id;
     private String nome;
     private String descricao;
     private String localizacao;
     private LocalDateTime horarioFuncionamento;
     private int capacidade;
-//    private List<TipoCozinha> tipoCozinha;
+    private List<TipoCozinha> tipoCozinha;
     private Proprietario proprietario;
 }
